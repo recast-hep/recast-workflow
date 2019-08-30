1978a155abd393270ea5d7cf3d418c7038656d5f
Alex Schuy <alexjschuy@gmail.com>
2019-08-30 08:58:21 -0700

    Fixes definitions. [skip travis]

--- definitions.py
+++ definitions.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).resolve().parent
-INTERFACE_DIR = ROOT_DIR / 'interfaces'
 TESTS_DIR = ROOT_DIR / 'tests'
 WORKFLOWS_DIR = ROOT_DIR / 'workflows'
-SUBWORKFLOWS_DIR = ROOT_DIR / 'subworkflows'
-IMAGES_DIR = ROOT_DIR / 'images'
+SRC_DIR = ROOT_DIR / 'recast-workflow'
+INTERFACE_DIR = SRC_DIR / 'interfaces'
+SUBWORKFLOWS_DIR = SRC_DIR / 'subworkflows'
+IMAGES_DIR = SRC_DIR / 'images'
